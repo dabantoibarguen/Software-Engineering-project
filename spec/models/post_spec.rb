79e373f5e30c5346b02a0f0c7b5f76b8c2dcf238
dabantoibarguen <78096139+dabantoibarguen@users.noreply.github.com>
2022-02-10 10:47:19 -0500

    Add files via upload

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+
+
+
+# context "User login test" do
+#   before :each do 
+#     @user = User.create!()
+#     @user = User.create!(email: 'admin@colgate.edu', password: 'testtest', admin: true)
+#     sign_in @user
+#     visit post_path
+  
+#   end
+
+
+#   it 'should not have a link to create a new blog post if user not signed in' do
+#   sign_out @user
+#   visit post_path
+#   expect(page).not_to have_content('Create new blog post')
+
+
+
+
+# end
+
+# it 'should redirect to the index if the new route is accessed and user is not signed in' do
+#   sign_out @user
+#   visit new_post_path
+#   expect(page.current_path).to eq(new_user_session_path)
+# end
+
+# end
+
+
+# context "the sort should exist" do
+#     expect(Post).to respond_to(:sorted_by)
+  
+  
+#   end
+
+#   it "should handle sorting by date" do
+#     expect(Post).to receive(:order).with(eq("date").or eq(:date))
+#     Product.sorted_by("date")
+#   end
+#end
+
+end 
+
+
+
+
+
